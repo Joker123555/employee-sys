6e42c17fb768dfbb81b54358713da756f3ea9b00
jane <3630911527@qq.com>
2024-09-10 17:18:57 +0800

    应用层

--- src/App.java
+++ src/App.java
@@ -4,6 +4,6 @@ import com.itheima.ui.LoginFrame;
 public class App {
     public static void main(String[] args) {
         new LoginFrame();
-        new EmployeeManager();
+//        new EmployeeManager();
     }
 }
